d7b002560f4236b48918bed2878433d4c17d3531
Luke Rickert <lukemusic@protonmail.com>
2026-02-17 12:04:22 -0600

    initial database code

--- database.py
+++ database.py
@@ -1,1 +1,18 @@
+import sqlite3
+
+#Each item will have the following attributes:
+    #name, description, quantity, category, date last updated, and location
+
+
+def createDatabase():
+    pass
+
+def addItem():
+    pass
+
+def removeItem():
+    pass
+
+def updateItem():
+    pass
 
