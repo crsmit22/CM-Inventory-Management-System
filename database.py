ef2f399630d3782bff1b89760c81a87c13ac2050
XManBoss6 <xav.steves@gmail.com>
2026-02-24 15:12:27 -0600

    Update database.py

--- database.py
+++ database.py
@@ -40,13 +40,14 @@ def search(searchWord): # Search function takes one search string and looks for 
 
     return rows
 
-def addItem(n, d, c, q, l):
+def addItem(n, d, c, q, l): # Function adds an element to the database
     with sqlite3.connect("inventoryDB.db") as conn:
         cursor = conn.cursor()
-    cursor.execute(
-        """INSERT INTO Inventory (Name, Description, Category, Quantity, Location, LastUpdated) VALUES 
-                   (n, d, c, q, l, date.today())"""
-    )
+        addQuery = """
+                INSERT INTO Inventory (Name, Description, Category, Quantity, Location, LastUpdated) VALUES 
+                   (?, ?, ?, ?, ?, ?)
+                """
+        cursor.execute(addQuery, (n, d, c, q, l, date.today()))
     #return print(f"Successfully added {n}")
 
 def removeItem(itemID): # Function takes item ID. Deletes item by ID.
@@ -60,6 +61,29 @@ def removeItem(itemID): # Function takes item ID. Deletes item by ID.
 
         cursor.execute(searchQuerey, (itemID))
 
-def updateItem():
-    pass
+def updateItem(itemID, n, d, c, q, l): # Function takes an item ID and updates said item
+    with sqlite3.connect("inventoryDB.db") as conn:
+        cursor = conn.cursor()
+        updateQuery = """
+            UPDATE Inventory
+            SET
+                name = ?,
+                description = ?,
+                category = ?,
+                quantity = ?,
+                location = ?,
+                LastUpdated = ?
+            WHERE ItemId = ?;
+        """
 
+        cursor.execute(updateQuery, (n, d, c, q, l, date.today(), itemID))
+    
+
+
+# """INSERT INTO Inventory (Name, Description, Category, Quantity, Location, LastUpdated) VALUES 
+#  ('Luna', 'Glass container to hold the Eucharist in a Monstrance', 'Sacred Vessels', 1, 'Tabernacle', 2026-02-22),
+#  ('Cruet', 'holds the water and the wine for Mass', 'Glassware', 2, 'Minifridge', 2026-01-20),
+#  ('Main Candle Extinguisher', 'extinguishes candles', null, 1, 'Sacristy Closet Door', 2026-01-20),
+#  ('Small hosts tub', 'unconsecrated hosts used for Mass, 1 3/8in', 'Supplies', 5, 'Hosts Cabinet', 2025-02-20),
+#  ('Purificator', 'used to clean sacred vessels', 'Linens', 15, 'Liturgy Linens', 2020-11-30),
+#  ('Lavabo Towel', 'used for Fr. to dry his wet hands before consecration', 'Linens', 2, 'Liturgy Linens', 2022-05-13);"""
