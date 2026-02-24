fe277532d05df175561cce75b8bd1de412f34918
XManBoss6 <xav.steves@gmail.com>
2026-02-24 13:57:41 -0600

    Update database.py

--- database.py
+++ database.py
@@ -7,7 +7,17 @@ import sqlite3
 def createDatabase():   # This function creates the database with its table
     with sqlite3.connect("inventoryDB.db") as conn:
         cursor = conn.cursor()
-
+        
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS Inventory(
+            ItemID INT PRIMARY KEY AUTOINCREMENT,
+            Name VARCHAR(50),
+            Description VARCHAR(30),
+            Category VARCHAR(30),
+            Quantity INT,
+            Location Varchar(30),
+            LastUpdated DATE
+            ) """
         
         
         cursor.execute(create_table_query)
